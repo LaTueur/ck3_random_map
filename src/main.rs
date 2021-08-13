e5e2f89a1c4aa853c403a7876a22552d2840aa27
LajosTueur <geonczeol.boti@gmail.com>
2021-08-13 15:20:32 +0200

    Based on heightmap and rivers map

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,14 @@ use rand::thread_rng;
 use std::time::SystemTime;
 
 const NUM_OF_COLORS:u32 = (255 as u32).pow(3);
-const LAND_MAP: &str = "mod/plain.png";
+const LAND_MAP: &str = "mod/heightmap.png";
 const PROVINCE_GRID_SIZE:u32 = 30;
+const LAND_COLOR: u8 = 16;
+const BLACK:im::Rgb<u8> = im::Rgb([0, 0, 0]);
+const WHITE:im::Rgb<u8> = im::Rgb([255, 255, 255]);
+const PINK:im::Rgb<u8> = im::Rgb([255, 0, 128]);
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 struct Coords{x: u32, y: u32}
 impl Coords{
     fn simple_distance(&self, other: &Coords) -> u32{
@@ -19,14 +23,21 @@ impl Coords{
     fn multi_distance(self: &Coords, coord2: &Coords, coord3: &Coords) -> u32{
         self.simple_distance(coord2) * self.simple_distance(coord3)
     }
+    fn as_index(&self, width: u32) -> u32{
+        self.x + self.y*width
+    }
 }
 
-trait NumToColor{
+trait NumAsType{
     fn as_rgb8(&self) -> im::Rgb<u8>;
+    fn as_coords(&self, width: u32) -> Coords;
 }
-impl NumToColor for u32{
+impl NumAsType for u32{
     fn as_rgb8(&self) -> im::Rgb<u8>{
         im::Rgb([(self%255) as u8, (self/255%255) as u8, (self/255/255) as u8])
+    }
+    fn as_coords(&self, width: u32) -> Coords{
+        Coords{x: self%width, y: self/width}
     }
 }
 
@@ -38,22 +49,56 @@ fn main() {
     let mut rng = thread_rng();
     colors.shuffle(&mut rng);
     let mut provinces: Vec<Vec<Coords>> = vec!();
+    let mut map_pixels: Vec<bool> = vec!();
+    let mut land_pixels: Vec<u32> = vec!();
+    let mut pixel_count: u32 = 0;
+    for pixel in map.pixels(){
+        if pixel[0] > LAND_COLOR{
+            map_pixels.push(true);
+            land_pixels.push(pixel_count);
+        }
+        else{
+            map_pixels.push(false);
+        }
+        pixel_count += 1;
+    }
+    for pixel in 0..pixel_count{
+        let coords = pixel.as_coords(width);
+        if map_pixels[pixel as usize]{
+            map.put_pixel(coords.x, coords.y, WHITE);
+        }
+        else{
+            map.put_pixel(coords.x, coords.y, PINK);
+        }
+    }
+    map.save("rivers.png").unwrap();
     for base_x in 0..width/PROVINCE_GRID_SIZE{
         for base_y in 0..height/PROVINCE_GRID_SIZE{
-            let coords1 = Coords{x: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_x*PROVINCE_GRID_SIZE,
-                                y: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_y*PROVINCE_GRID_SIZE};
-            let coords2 = Coords{x: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_x*PROVINCE_GRID_SIZE,
-                                y: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_y*PROVINCE_GRID_SIZE};
-            provinces.push(vec!(coords1, coords2));
+            let mut valid_pixels:Vec<Coords> = vec!();
+            for x in 0..PROVINCE_GRID_SIZE{
+                for y in 0..PROVINCE_GRID_SIZE{
+                    let coords = Coords{x: x+base_x*PROVINCE_GRID_SIZE, y: y+base_y*PROVINCE_GRID_SIZE};
+                    let index = coords.as_index(width);
+                    if map_pixels[index as usize]{
+                        valid_pixels.push(coords);
+                    }
+                }
+            }
+            if valid_pixels.len() > 1{
+                provinces.push(vec!(*valid_pixels.choose(&mut rng).unwrap(), *valid_pixels.choose(&mut rng).unwrap()));
+            }
         }
+    }
+    for i in land_pixels{
+        let coords = i.as_coords(width);
+        provinces.iter_mut().min_by(|a, b|
+            coords.multi_distance(&a[0], &a[1])
+            .cmp(&coords.multi_distance(&b[0], &b[1]))
+        ).unwrap().push(coords);
     }
     for x in 0..width{
         for y in 0..height{
-            let coords = Coords{x, y};
-            provinces.iter_mut().min_by(|a, b|
-                coords.multi_distance(&a[0], &a[1])
-                .cmp(&coords.multi_distance(&b[0], &b[1])
-            )).unwrap().push(coords);
+            map.put_pixel(x, y, BLACK);
         }
     }
     for i in 0..provinces.len(){
@@ -62,6 +107,7 @@ fn main() {
             map.put_pixel(coords.x, coords.y, color);
         }
     }
-    map.save("test.png").unwrap();
+    map.save("provinces.png").unwrap();
+
     println!("{}", SystemTime::now().duration_since(start_time).unwrap().as_millis().to_string());
 }
