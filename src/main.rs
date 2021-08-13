8d009a474b10a7b7cd5fde73bdcc6705211ae212
LajosTueur <geonczeol.boti@gmail.com>
2021-08-13 11:44:08 +0200

    First working version

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,17 @@ use rand::thread_rng;
 
 const NUM_OF_COLORS:u32 = (255 as u32).pow(3);
 const LAND_MAP: &str = "mod/plain.png";
-const PROVINCE_GRID_SIZE:u32 = 20;
+const PROVINCE_GRID_SIZE:u32 = 30;
+
+struct Coords{x: u32, y: u32}
+impl Coords{
+    fn simple_distance(&self, other: &Coords) -> u32{
+        (cmp::max(self.x, other.x) - cmp::min(self.x, other.x))+(cmp::max(self.y, other.y) - cmp::min(self.y, other.y))
+    }
+    fn multi_distance(self: &Coords, coord2: &Coords, coord3: &Coords) -> u32{
+        self.simple_distance(coord2) * self.simple_distance(coord3)
+    }
+}
 
 trait NumToColor{
     fn as_rgb8(&self) -> im::Rgb<u8>;
@@ -19,36 +29,34 @@ impl NumToColor for u32{
     }
 }
 
-fn get_distance((x1, y1): (u32, u32), (x2, y2): (u32, u32)) -> u64{
-    (((cmp::max(x1, x2) - cmp::min(x1, x2))*(cmp::max(y1, y2) - cmp::min(y1, y2))) as f64).sqrt() as u64
-}
-
 fn main() {
     let mut map = im::open(LAND_MAP).unwrap().into_rgb8();
     let (width, height) = (map.width(), map.height());
     let mut colors: Vec<u32> = (0..NUM_OF_COLORS).collect();
     let mut rng = thread_rng();
     colors.shuffle(&mut rng);
-    let mut provinces: Vec<Vec<(u32, u32)>> = vec!();
+    let mut provinces: Vec<Vec<Coords>> = vec!();
     for base_x in 0..width/PROVINCE_GRID_SIZE{
         for base_y in 0..height/PROVINCE_GRID_SIZE{
-            let coords = (rng.gen_range(0..PROVINCE_GRID_SIZE)+base_x*PROVINCE_GRID_SIZE,
-                            rng.gen_range(0..PROVINCE_GRID_SIZE)+base_y*PROVINCE_GRID_SIZE);
-            provinces.push(vec!(coords));
+            let coords1 = Coords{x: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_x*PROVINCE_GRID_SIZE,
+                                y: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_y*PROVINCE_GRID_SIZE};
+            let coords2 = Coords{x: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_x*PROVINCE_GRID_SIZE,
+                                y: rng.gen_range(0..PROVINCE_GRID_SIZE)+base_y*PROVINCE_GRID_SIZE};
+            provinces.push(vec!(coords1, coords2));
         }
     }
     for x in 0..width{
         for y in 0..height{
-            let coords = (x, y);
-            provinces.sort_by(|a, b| get_distance(coords, a[0])
-                    .cmp(&get_distance(coords, b[0])));
+            let coords = Coords{x, y};
+            provinces.sort_by(|a, b| coords.multi_distance(&a[0], &a[1])
+                    .cmp(&coords.multi_distance(&b[0], &b[1])));
             provinces[0].push(coords);
         }
     }
     for i in 0..provinces.len(){
         let color = colors[i].as_rgb8();
-        for (x, y) in provinces[i].iter(){
-            map.put_pixel(*x, *y, color)
+        for coords in provinces[i].iter(){
+            map.put_pixel(coords.x, coords.y, color);
         }
     }
     map.save("test.png").unwrap();
