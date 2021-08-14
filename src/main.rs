65512f289341a9f2c05d05ced1cf4ea971b8d74c
LajosTueur <geonczeol.boti@gmail.com>
2021-08-14 15:30:14 +0200

    Actual minimum for working map

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ extern crate image as im;
 
 use std::cmp;
 use rand::seq::SliceRandom;
-use rand::Rng;
+//use rand::Rng;
 use rand::thread_rng;
 use std::time::SystemTime;
+use std::path::Path;
+use std::fs;
+use std::io::Write;
 
 const NUM_OF_COLORS:u32 = (255 as u32).pow(3);
-const LAND_MAP: &str = "mod/heightmap.png";
+const LAND_MAP: &str = "from/heightmap.png";
 const PROVINCE_GRID_SIZE:u32 = 30;
-const LAND_COLOR: u8 = 16;
+const LAND_COLOR: u8 = 0;
 const BLACK:im::Rgb<u8> = im::Rgb([0, 0, 0]);
 const WHITE:im::Rgb<u8> = im::Rgb([255, 255, 255]);
 const PINK:im::Rgb<u8> = im::Rgb([255, 0, 128]);
+const FOLDERS:[&str; 2] = ["map_data", "common/landed_titles"];
+const ROOT_FOLDER:&str = "mod";
 
 #[derive(PartialEq, Clone, Copy)]
 struct Coords{x: u32, y: u32}
@@ -52,6 +57,11 @@ fn main() {
     let mut map_pixels: Vec<bool> = vec!();
     let mut land_pixels: Vec<u32> = vec!();
     let mut pixel_count: u32 = 0;
+    for i in FOLDERS.iter(){
+        let path = Path::new(ROOT_FOLDER).join(Path::new(i));
+        fs::create_dir_all(path).unwrap();
+    }
+    map.save("mod/map_data/heightmap.png").unwrap();
     for pixel in map.pixels(){
         if pixel[0] > LAND_COLOR{
             map_pixels.push(true);
@@ -71,7 +81,7 @@ fn main() {
             map.put_pixel(coords.x, coords.y, PINK);
         }
     }
-    map.save("rivers.png").unwrap();
+    map.save("mod/map_data/rivers.png").unwrap();
     for base_x in 0..width/PROVINCE_GRID_SIZE{
         for base_y in 0..height/PROVINCE_GRID_SIZE{
             let mut valid_pixels:Vec<Coords> = vec!();
@@ -101,13 +111,32 @@ fn main() {
             map.put_pixel(x, y, BLACK);
         }
     }
+    let mut definition = String::from("0;0;0;0;x;x;\n");
+    let mut titles = String::from("e_test = { color = { 0 0 0 } color2 = { 255 255 255 } capital = c_test_1 k_test = { color = { 0 0 0 } color2 = { 255 255 255 \n");
     for i in 0..provinces.len(){
         let color = colors[i].as_rgb8();
+        let index = i+1;
+        definition.push_str(&format!("{index};{};{};{};b_test_{index};x;\n", color[0], color[1], color[2], index=index));
+        if i%3 == 0{
+            if i != 0{
+                titles.push_str(&"}");
+            }
+            if i/3%3 == 0{
+                titles.push_str(&format!(" }} \n d_test_{index} = {{ color = {{ {} {} {} }} color2 = {{ 255 255 255 }} capital = c_test_{index}\n", color[0], color[1], color[2], index=index));
+            }
+            titles.push_str(&format!("c_test_{index} = {{ color = {{ {} {} {} }} color2 = {{ 255 255 255 }}\n", color[0], color[1], color[2], index=index));
+        }
+        titles.push_str(&format!("b_test_{index} = {{ province = {index} color = {{ {} {} {} }} color2 = {{ 255 255 255 }} }}\n", color[0], color[1], color[2], index=index));
         for coords in provinces[i].iter(){
             map.put_pixel(coords.x, coords.y, color);
         }
     }
-    map.save("provinces.png").unwrap();
+    titles.push_str(&"} } } }");
+    map.save("mod/map_data/provinces.png").unwrap();
+    let mut file = fs::File::create("mod/map_data/definition.csv").unwrap();
+    file.write_all(definition.as_bytes()).unwrap();
+    let mut file = fs::File::create("mod/common/landed_titles/00_landed_titles.txt").unwrap();
+    file.write_all(titles.as_bytes()).unwrap();
 
     println!("{}", SystemTime::now().duration_since(start_time).unwrap().as_millis().to_string());
 }
